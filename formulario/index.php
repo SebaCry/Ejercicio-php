37757e65f715e375cab103f673b99993ce6af228
zuskay <juansebastianpulido05@gmail.com>
2025-04-29 16:32:57 -0500

    .

--- formulario/index.php
+++ formulario/index.php
@@ -4,9 +4,9 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link rel="stylesheet" href="/ejerciciophp/Ejercicio-php/formulario/style.css">
-
   <title>DCARHTL.DCI</title>
 </head>
+<body>
 
   <header>
     <div class="logo">DCARHTL.DCI Consultoría</div>
@@ -15,44 +15,49 @@
       <a href="#">Servicios</a>
       <a href="#">Contacto</a>
       <a href="tel:+525547740586">55 4774 0586</a>
-      <a href="#">📘</a>
-      <a href="#">🐦</a>
+      <img src="/ejerciciophp/Ejercicio-php/img/facebook.png" class="iconos"><img/>
+      <img src="/ejerciciophp/Ejercicio-php/img/correo.png" class="iconos"></img>
     </nav>
   </header>
 
   <main>
     <section class="hero">
       <div class="left-content">
-        <h1>Somos un grupo <br><span>de especialistas</span></h1>
+        <h1>Somos un grupo<br><span class="span">de especialistas</span></h1>
         <p>en todo lo relacionado a la materia del trabajo.</p>
         <div class="image-box">
-          <img src="https://via.placeholder.com/600x300" alt="Asesoría gratuita"/>
+          <img src="/ejerciciophp/Ejercicio-php/img/img2.png" style="width: 450px;" alt="Asesoría gratuita"/>
           <span>BRINDAMOS ASESORÍA GRATUITA</span>
         </div>
       </div>
-      <div class="right-content">
-        <div class="contact-form">
-          <h2>Contáctanos para conocer más de nuestros servicios.</h2>
-          <form action="procesar.php" method="POST">
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" required>
-        <br>
-        <label for="email">Email:</label>
-        <input type="email" name="email" required>
-        <br>
-        <label for="telefono">Teléfono:</label>
-        <input type="text" name="telefono" required>
-        <br>
-        <label for="mensaje">Mensaje:</label>
-        <input type="text" name="mensaje" required>
-        <br>
-        <button type="submit">Enviar</button>
-        </form>
-        <br>
-        <a href="mostrar.php">Ver Registros</a>
+      <form action="">
+        <div class="right-content">
+          <div class="contact-form">
+            <h2>Contáctanos para conocer más de nuestros servicios.</h2>
+            <form action="procesar.php" method="POST">
+                <label for="nombre">Nombre:</label>
+                <input type="text" name="nombre" required>
+                <br>
+                <label for="email">Email:</label>
+                <input type="email" name="email" required>
+                <br>
+                <label for="telefono">Teléfono:</label>
+                <input type="text" name="telefono" required>
+                <br>
+                <label for="mensaje">Mensaje:</label>
+                <input type="text" name="mensaje" required>
+                <br>
+                <button type="submit">Enviar</button>
+            </form>
+          </div>
+          <div class="side-button">MÁS INFORMACIÓN</div>
         </div>
-        <div class="side-button">MÁS INFORMACIÓN</div>
-      </div>
+      </form>
     </section>
   </main>
-</html>
+  
+  
+
+
+
+</body>
